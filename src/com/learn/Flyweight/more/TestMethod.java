824b171cdf6691032d7c9b8703aacf7ebd10efa6
jzwgit <706861648@qq.com>
2018-09-20 17:24:30 +0800

    .

--- src/com/learn/Flyweight/more/TestMethod.java
+++ src/com/learn/Flyweight/more/TestMethod.java
@@ -9,5 +9,6 @@ public class TestMethod {
         ShareFactory factory = new ShareFactory();
         Share share = factory.factory("东西南",'1');
         System.out.println("内蕴东西南不同，外蕴君威字体："+share.work("宋体"));
+        System.out.print("这是我用手机写出来了明眸更名你");
     }
 }
